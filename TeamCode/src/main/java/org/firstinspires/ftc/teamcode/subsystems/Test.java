756d6825566249dd1de13ef96c5e79387baad21e
Nathan Xie <107293298+NathanXie6@users.noreply.github.com>
2024-07-24 14:30:00 -0700

    testing pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Test.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -9,16 +11,26 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import android.util.Log;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.libswerve.PID;
 import org.firstinspires.ftc.teamcode.utils.Encoder;
 
+@Config
 @TeleOp
 public class Test extends LinearOpMode {
     public CRServo servo;
     private long start;
+
+    public static PID test = new PID(1, 0, 0.5);
+
+    public static int desiredPosition = 200;
+
+    FtcDashboard dashboard;
     @Override
     public void runOpMode() throws InterruptedException {
-        servo = hardwareMap.get(CRServo.class, "test");
+//        servo = hardwareMap.get(CRServo.class, "test");
 
+
+        dashboard = FtcDashboard.getInstance();
 
         DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
         DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
@@ -30,12 +42,12 @@ public class Test extends LinearOpMode {
         backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        servo.setDirection(DcMotorSimple.Direction.REVERSE);
+        //servo.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
         if (isStopRequested()) return;
 
@@ -48,73 +60,29 @@ public class Test extends LinearOpMode {
             double initAccel = 0;
             double now = System.currentTimeMillis();
 
-            int desiredPosition = 537;
+
+            double command = test.getOut(desiredPosition - frontLeftMotor.getCurrentPosition());
             frontLeftMotor.setTargetPosition(desiredPosition);
-            frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            frontLeftMotor.setPower(0.2);
+            frontLeftMotor.setPower(command);
+
+//            frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
 
             double position = frontLeftMotor.getCurrentPosition();
-//            double velocity = frontLeftMotor.getVelocity();
-//
-//            double elMillis =  now - prev;
-//            double elSec = elMillis / 1000;
-//            double elVelo = velocity - prevVelo;
-//            double acceleration = elVelo/ elSec;
-//            initAccel = acceleration;
-//            prevVelo = velocity;
+            double velocity = frontLeftMotor.getVelocity();
 
+
+            telemetry.addData("Velocity", velocity);
             telemetry.addData("Encoder Position", position);
             telemetry.addData("Desired Position", desiredPosition);
 
             telemetry.update();
 
 
-            //code for deriving velocity from position (old and not used)
-//            double now = System.currentTimeMillis();
-//
-//            frontLeftMotor.setPower(0.1);
-//            int newPos = frontLeftMotor.getCurrentPosition();
-//
-//            testMotor.setPower(0.1);
-//            poopMotor.setPower(0.1);
-//
-//            double elMillis =  now - prev;
-//            double elSec = elMillis / 1000;
-//            double elPos = newPos - prevPos;
-//            double velocity = elPos/ elSec;
-//
-//            prev = now;
-//            prevPos = newPos;
-//
-//            Log.i("elTime", String.valueOf(elPos));
 
-//            double time_curr = System.currentTimeMillis();
-//            double time_new = 0;
-//            while(time_curr < )
-
-            // servo.setDirection(DcMotorSimple.Direction.FORWARD);
-
-//            servo.setPower(-1);
         }
     }
 
-//    public void pause (double milliseconds) {
-//        start = System.currentTimeMillis();
-//        while (System.currentTimeMillis() - start < milliseconds && opModeIsActive()) {
-//            servo.update();
-//        }
-//    }
-//
-//    public void update() {
-//        START_LOOP();
-//        Globals.gotBloodyAnnihilated = System.currentTimeMillis() - Globals.autoStartTime > 29500 && Globals.autoStartTime != -1 && deposit.slides.length > 1;
-//        updateSubsystems();
-//        updateTelemetry();
-//    }
-
-    //testing everytioasdfkhasdf
-    //testing push
-    //testing push clone
 
     //pw xie1017
 }
